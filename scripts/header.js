89e1f3e5d2d5d41601064216d2f000568b0481f3
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-22 01:44:35 +0800

    Finish animation of header elements

---
+++ scripts/header.js
@@ -0,0 +1,68 @@
+$(function() {
+    const timer = 180;
+
+    $('#logo-img').attr('src', 'assets/logo.png');
+    $('#title-text').text('10111010101');
+    $('#title-text').css('color', 'rgb(240, 240, 119)');
+
+    setTimeout(
+        function() {
+            $('#title-text').text('01110101010');
+            $('#title-text').css('color', 'white');
+        }, timer 
+    )
+
+    setTimeout(
+        function() {
+            $('#logo-img').attr('src', 'assets/logo-yellow.png');
+            $('#title-text').text('00101101010');
+            $('#title-text').css('color', 'rgb(240, 240, 119)');
+        }, 2 * timer
+    )
+
+    setTimeout(
+        function() {
+            $('#title-text').text('110101010101');
+            $('#title-text').css('color', 'white');
+        }, 3 * timer
+    )
+
+    setTimeout(
+        function() {
+            $('#logo-img').attr('src', 'assets/logo.png');
+            $('#title-text').text('100101010011');
+            $('#title-text').css('color', 'rgb(240, 240, 119)');
+        }, 4 * timer
+    )
+
+    setTimeout(
+        function() {
+            $('#title-text').text('110001110010');
+            $('#title-text').css('color', 'white');
+        }, 5 * timer
+    )
+
+    setTimeout(
+        function() {
+            $('#logo-img').attr('src', 'assets/logo-yellow.png');
+            $('#title-text').html('BINARY &nbsp;MULTIPLICAT<span class = "code">10</span>N');
+            $('.code').css('font-size', '18px');
+        }, 6 * timer
+    )
+
+    setTimeout(
+        function() {
+            $('#logo-img').attr('src', 'assets/logo.png');
+           
+            $('#title-text').html('BINARY &nbsp;MULTIPLICATION');
+        }, 9 * timer
+    )
+
+    $('#logo-img').on('mouseover', function() {
+        $('#logo-img').attr('src', 'assets/logo-yellow.png');
+    });
+
+    $('#logo-img').on('mouseout', function() {
+        $('#logo-img').attr('src', 'assets/logo.png');
+    });
+});
