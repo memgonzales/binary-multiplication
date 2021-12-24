d8e689bdd9f1f7b727beb2db643447e926631926
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-24 16:12:46 +0800

    Start with demo (results)

---
+++ scripts/algo-strings.js
@@ -0,0 +1,31 @@
+/* --- TRIVIA HEADER --- */
+const pencilTriviaHeader = '. ';
+const boothsTriviaHeader = '. ';
+const extendedBoothsTriviaHeader = 'Triple Treat. ';
+
+const triviaHeaders = [pencilTriviaHeader, boothsTriviaHeader, extendedBoothsTriviaHeader];
+
+/* --- TRIVIA --- */
+const pencilTrivia = 
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Proin in nisl nec metus tristique mattis. Vestibulum ante ipsum primis in faucibus orci luctus 
+et ultrices posuere cubilia curae;`
+
+const boothsTrivia = ``;
+
+const extendedBoothsTrivia = 
+    `In 2008, an extension of Booth's algorithm for multiplying three operands on FPGAs 
+was proposed by Israeli researchers Yosi Ben Asher and Esti Stein&nbsp; 
+<a class = "no-decor no-hover trivia" href = "https://ieeexplore.ieee.org/document/4762411" target = "_blank">
+<i class = "fa fa-external-link no-hover"></i></a>`
+
+const trivia = [pencilTrivia, boothsTrivia, extendedBoothsTrivia];
+
+/* --- ALGORITHM --- */
+const pencilAlgo = ``;
+const boothsAlgo = ``;
+const extendedBoothsAlgo = 
+    `1. Represent the operands in two's complement using the fewest number of bits. <br>
+2. Perform sign extension`
+
+const algo = [pencilAlgo, boothsAlgo, extendedBoothsAlgo];
--- scripts/extended-booths.js
+++ scripts/extended-booths.js
@@ -1,3 +1,3 @@
 $(function() {
-    showTriviaExtendedBooths();
+    showTrivia('extended-booths');
 });
