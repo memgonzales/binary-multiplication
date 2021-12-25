f8113af6317e2569c2c4006affe3de6b5a73baa0
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-25 15:51:51 +0800

    Implement demo switching based on multiplication method

---
+++ scripts/demo-util.js
@@ -0,0 +1,15 @@
+function demo() {
+    $('#multiply').on('click', function() {
+        $('#algo-name').html('');
+        $('#algo-steps').html('');
+
+        switch($('#algo-value').text()) {
+            case 'Pencil-and-Paper Method':
+                break;
+            case 'Booth\'s Algorithm':
+                break;
+            case 'Extended Booth\'s Algorithm': 
+                break;
+        }
+    });
+}
