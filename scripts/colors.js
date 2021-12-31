82077a3e7c0cbe4e4f6c96136cd18354ad822e77
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-31 13:49:07 +0800

    Fetch colors in JS file from CSS root variables

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,10 +1,10 @@
 /**
  * File containing the colors used in the website.
- *
- * The RGB values should match the specifications in colors.css.
  */
 
-const green = 'rgb(61, 83, 33)';
-const gray = 'rgb(177, 137, 137)';
-const yellow = 'rgb(240, 240, 119)';
+const docStyle = getComputedStyle(document.documentElement);
+
+const green = docStyle.getPropertyValue('--green-emphasis');
+const gray = docStyle.getPropertyValue('--gray-emphasis');
+const yellow = docStyle.getPropertyValue('--yellow-emphasis');
 const white = 'white';
